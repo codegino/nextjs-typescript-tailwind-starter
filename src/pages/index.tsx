fce9f00186b3b9ffcd7b93a00c58f31f24ba1092
codegino <carloginocatapang@gmail.com>
2022-05-25 20:47:37 +0800

    chore: Added Tailwind CSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,9 @@ import Layout from "../components/Layout";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+    <h1 className="text-3xl font-bold underline">
+      Hello Next.js with TypeScript and Tailwind 👋
+    </h1>
     <p>
       <Link href="/about">
         <a>About</a>
